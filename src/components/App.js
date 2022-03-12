44a0964239c22f3ef4ff60c26ad3cd1535e59569
MichaelR <spider747@gmail.com>
2022-03-12 21:06:59 +0200

    Add Footer section

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <h1>Start</h1>
+      <Footer />
     </div>
   );
 }
