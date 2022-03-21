2dbcca38434b714eabbe9615cb4d88ca25eed922
MichaelR <spider747@gmail.com>
2022-03-21 23:03:04 +0200

    prevent default form behavior

--- src/components/CreateNoteForm.jsx
+++ src/components/CreateNoteForm.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 function CreateNoteForm() {
+
+
+	function submitNote (submitEvent) {
+
+		submitEvent.preventDefault();				// to prevent refresh of page
+	}
+
   return (
     <div>
-      <form>
+      <form onSubmit={submitNote}>
         <input name='title' placeholder='Title'/>
 				<input name='note' placeholder='Write note here'/>
 				<button type='submit'>Add</button>
