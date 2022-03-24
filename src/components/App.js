02f633d12750aee955495f376679aff965949649
MichaelR <spider747@gmail.com>
2022-03-24 21:47:41 +0200

    add delete note function

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import CreateNoteForm from './CreateNoteForm';
@@ -6,30 +6,32 @@ import Note from './Note';
 import dummyNotes from '../dummyData';
 import './App.css';
 
-
 function App() {
-
   var [notes, setNotes] = useState(dummyNotes);
 
-  function addNote (singleNote) {
+  function addNote(singleNote) {
     console.log(singleNote);
     setNotes((prevNotes) => {
       console.log(prevNotes);
-      return [...prevNotes, singleNote]
+      return [...prevNotes, singleNote];
     });
   }
 
-  function deleteNote () {
-    
+  function deleteNote(noteId) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((item) => {
+        return item.id !== noteId;
+      })
+    });
   }
 
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
-      <CreateNoteForm addNote={addNote}/>
-      <div className="container">
+      <CreateNoteForm addNote={addNote} />
+      <div className='container'>
         {notes.map((item) => {
-          return <Note key={item.id} {...item}/>
+          return <Note key={item.id} {...item} onDelete={deleteNote} />;
         })}
       </div>
       <Footer />
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 
-function Note ({title, note}) {
+function Note (props) {
+
+    function handleDeleteClick () {
+        props.onDelete(props.id);
+    }
+
     return (
         <div className='note'>
-            <h1>{title}</h1>
-            <p>{note}</p>
+            <h1>{props.title}</h1>
+            <p>{props.note}</p>
+            <button onClick={handleDeleteClick}>X</button>
         </div>
     )
 }
